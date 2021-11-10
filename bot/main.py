a666e0403b687b33eb3cc8d2398fa25be1a0b45c
3DG <72684473+3DG@users.noreply.github.com>
2021-11-10 16:24:26 -0500

    added code

--- bot/main.py
+++ bot/main.py
@@ -1,1 +1,9 @@
-
+import discord
+from discord.ext import commands as cmds
+token = "TOKEN GOES HERE"
+bot = cmds.Bot(command_prefix="!")
+@Bot.commands()
+async def ping(ctx):
+  await ctx.send("Pong!")
+  
+bot.run(token)
