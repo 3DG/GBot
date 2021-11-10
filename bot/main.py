2f53eaa49cd413e6800ad8f247debc8fd7cbbac0
3DG <72684473+3DG@users.noreply.github.com>
2021-11-10 17:54:40 -0500

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,9 +1,20 @@
 import discord
+import time as clock
+import os
+import math
+from time import mktime
 from discord.ext import commands as cmds
 token = "TOKEN GOES HERE"
 bot = cmds.Bot(command_prefix="!")
-@Bot.commands()
+@bot.command()
 async def ping(ctx):
-  await ctx.send("Pong!")
+  time = ctx.message.created_at
+  timenow = mktime(clock.gmtime())
+  msnow = math.floor(clock.time()*1000)/1000 - math.floor(clock.time())
+  timenow += msnow
+  print((timenow))
+  print((time.timestamp()))
+  timesend = math.floor(((timenow) - (time.timestamp()))*-1000)
+  await ctx.send(':ping_pong: Pong! ('+str(timesend)+' ms)')
   
-bot.run(token)
+bot.run(token) 
