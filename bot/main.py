d53cb20e92a12690c7e5a853ee94b5c577d7d8d4
3DG <72684473+3DG@users.noreply.github.com>
2021-11-13 12:17:34 -0500

    fixed endswith and pixel command

--- bot/main.py
+++ bot/main.py
@@ -9,7 +9,7 @@ from io import BytesIO as toimg
 from PIL import Image
 from time import mktime
 from discord.ext import commands as cmds
-token = ""
+token = "" #put your bot token here
 prefix = "g!"
 bot = cmds.Bot(command_prefix=prefix,help_command=None)
 
@@ -36,51 +36,70 @@ async def ping(ctx):
 async def jpegify(ctx, quality=5):
   if ctx.message.attachments != []:
     url = ctx.message.attachments[0].url
-    imgdata = requests.get(url)
-    img = Image.open(toimg(imgdata.content))
-    fileName = randomStr(24)
-    img = img.convert("RGB")
-    img.save("./temp/img/"+fileName+"jpgified.jpg", mode="JPEG", quality=quality)
-    file = open("./temp/img/"+fileName+"jpgified.jpg", "rb", buffering = 0)
-    await ctx.send("Here is your image!", file=discord.File(file, filename="convertedimage.jpg"))
-    file.close()
-    os.remove("./temp/img/"+fileName+"jpgified.jpg")
-  else:
-    await ctx.send("This command requires an image!")
-
-#jpeg image command
-@bot.command()
-async def pixel(ctx, scalewid=32, scalehgt=None):
-  if ctx.message.attachments != []:
-    if scalehgt == None:
-      scalehgt = scalewid
-    if scalewid < 1 or scalehgt < 1:
-      await ctx.send("Scale factor must be more than 1")
-    else:
-      url = ctx.message.attachments[0].url
+    if url.endswith(".png") or url.endswith(".jpg") or url.endswith(".gif"):
       imgdata = requests.get(url)
       img = Image.open(toimg(imgdata.content))
       fileName = randomStr(24)
-      img = img.convert("RGBA")
-      wid, hgt = img.size
-      img = img.resize((math.floor(wid/scalewid)+1,math.floor(hgt/scalehgt)+1),resample=Image.BILINEAR)
-      img = img.resize((wid,hgt), resample=Image.BILINEAR)
-      img.save("./temp/img/"+fileName+"pixel.png")
-      file = open("./temp/img/"+fileName+"pixel.png", "rb", buffering = 0)
-      await ctx.send("Here is your image!", file=discord.File(file, filename="convertedimage.png"))
+      img = img.convert("RGB")
+      img.save("./temp/img/"+fileName+"jpgified.jpg", mode="JPEG", quality=quality)
+      file = open("./temp/img/"+fileName+"jpgified.jpg", "rb", buffering = 0)
+      await ctx.send("Here is your image!", file=discord.File(file, filename="convertedimage.jpg"))
       file.close()
-      os.remove("./temp/img/"+fileName+"pixel.png")
+      os.remove("./temp/img/"+fileName+"jpgified.jpg")
+    else:
+      await ctx.send("Your image must be a PNG, JPG, or GIF image.")
   else:
     await ctx.send("This command requires an image!")
-    
-#echo command
-@bot.command()
-async def echo(ctx):
-  await ctx.send(str(ctx.message.content)[(4 + len(prefix)) : len(str(ctx.message.content))])
-  await ctx.message.delete()
 
+#caption img command
+async def caption(ctx, caption):
+  if ctx.message.attachments != []:
+    url = ctx.message.attachments[0].url
+    if url.endswith(".png") or url.endswith(".jpg") or url.endswith(".gif"):
+      imgdata = requests.get(url)
+      img = Image.open(toimg(imgdata.content))
+      fileName = randomStr(24)
+      img = img.convert("RGB")
+      img.save("./temp/img/"+fileName+"jpgified.jpg", mode="JPEG", quality=quality)
+      file = open("./temp/img/"+fileName+"jpgified.jpg", "rb", buffering = 0)
+      await ctx.send("Here is your image!", file=discord.File(file, filename="convertedimage.jpg"))
+      file.close()
+      os.remove("./temp/img/"+fileName+"jpgified.jpg")
+    else:
+      await ctx.send("Your image must be a PNG, JPG, or GIF image.")
+  else:
+    await ctx.send("This command requires an image!")
+
+#pixel image command
+@bot.command()
+async def pixel(ctx, scalewid=24.0, scalehgt=None):
+  if ctx.message.attachments != []:
+    if scalehgt == None:
+      scalehgt = scalewid
+    if scalewid < 1.0 or scalehgt < 1.0:
+      await ctx.send("Scale factor must be more than 1")
+    else:
+      url = ctx.message.attachments[0].url
+      if url.endswith(".png") or url.endswith(".jpg") or url.endswith(".gif"):
+        imgdata = requests.get(url)
+        img = Image.open(toimg(imgdata.content))
+        fileName = randomStr(24)
+        img = img.convert("RGBA")
+        wid, hgt = img.size
+        img = img.resize((round(wid/scalewid)+1,round(hgt/scalehgt)+1),resample=Image.BILINEAR)
+        img = img.resize((wid,hgt), resample=Image.NEAREST)
+        img.save("./temp/img/"+fileName+"pixel.png")
+        file = open("./temp/img/"+fileName+"pixel.png", "rb", buffering = 0)
+        await ctx.send("Here is your image!", file=discord.File(file, filename="convertedimage.png"))
+        file.close()
+        os.remove("./temp/img/"+fileName+"pixel.png")
+      else:
+        await ctx.send("Your image must be a PNG, JPG, or GIF image.")
+  else:
+    await ctx.send("This command requires an image!")
 #help command
 helpdef = {"pixel":"Requires an image. Lowers the resolution of an image and scales it back up (Arguments: [Scale X factor, Scale Y factor])",
+           "echo":"Make the bot say anything! (Arguments: {Message})",
            "jpegify":"Requires an image. Returns a low quality jpg of the image sent (Arguments: [Quality])",
            "help":"Shows a list of commands",
            "ping":"Gets your ping to the bot"}
@@ -92,6 +111,11 @@ async def help(ctx, cmdorpage=0):
     send += "`g!"+str(cmd)+"`" + " - " + helpdef.get(str(cmd))+'''\n'''
   await ctx.send(send)
 
+#echo command
+@bot.command()
+async def echo(ctx):
+  await ctx.send(str(ctx.message.content)[(4 + len(prefix)) : len(str(ctx.message.content))])
+  await ctx.message.delete()
 
 #filter-chat in 3dg's discord
 def filter(message):
@@ -115,4 +139,3 @@ async def on_message(ctx):
       await ctx.delete()
   await bot.process_commands(ctx)
 bot.run(token) 
- 
